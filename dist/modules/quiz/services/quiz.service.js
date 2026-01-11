a2aa1b8f5293def1410c13a5ff220c33504bc8e1
Paras <paras@Paras-Kumar.local>
2026-01-11 17:03:03 +0530

    f

--- dist/modules/quiz/services/quiz.service.js
+++ dist/modules/quiz/services/quiz.service.js
@@ -174,10 +174,12 @@ let QuizService = class QuizService {
         const totalNoOfQuestions = await this.questionModel.countDocuments();
         if (alreadyAskedQuestionIds.length ===
             Math.min(this.totalNoOfQuestionToBeAsked, totalNoOfQuestions)) {
-            this.eventEmitter.emit('quiz.report.sendMail', {
-                quiz_id,
-                user_id,
-            });
+            setTimeout(() => {
+                this.eventEmitter.emit('quiz.report.sendMail', {
+                    quiz_id,
+                    user_id,
+                });
+            }, 10000);
         }
         return {
             message: 'Answer Submitted',
